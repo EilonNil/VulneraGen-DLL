4ebf62a87c2439c6abe37a5d1e078fb39c0a3b6b
Eilon Aspir <eilon.aspir@gmail.com>
2023-06-07 16:08:18 +0300

    7/6 fixed so it will print only on opened notepad

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,19 +1,38 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include <windows.h>
 #include <iostream>
+#include <string>
 
-extern "C" __declspec(dllexport) VOID printMessage(std::string message) {
-    HWND notepadHandle = FindWindowA(NULL, "Untitled - Notepad");
-    if (notepadHandle == NULL) {
-        notepadHandle = FindWindowA(NULL, "*Untitled - Notepad");
+HWND notepadHandle = NULL;
+
+BOOL CALLBACK EnumWindowsProcMy(HWND hwnd, LPARAM lParam)
+{
+    //this function is the base function that will find the HWND of the
+    //injected notepad using it's process ID.
+    DWORD lpdwProcessId;
+    GetWindowThreadProcessId(hwnd, &lpdwProcessId);
+    if (lpdwProcessId == lParam)
+    //if current process is the same as wanted process
+    {
+        notepadHandle = hwnd;
+        return FALSE;
     }
-    
+    return TRUE;
+}
+
+
+extern "C" __declspec(dllexport) VOID printMessage(std::string message, DWORD procID) {
+    EnumWindows(EnumWindowsProcMy, procID);
+    //go over all the windows and find the HWND of the notepad.
+
     HWND editControlHandle = FindWindowEx(notepadHandle, NULL, L"Edit", NULL);
     for (int i = 0; i < message.size(); i++) {
         const char character = message[i];
         PostMessage(editControlHandle, WM_CHAR, (WPARAM)character, 0);
         Sleep(50);
     }
+    //print the text by sending a message to print each letter with a tiny break in between to
+    //make it look flowing like human typing.
 }
 
 
